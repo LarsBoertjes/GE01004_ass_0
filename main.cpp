b4780b70a8c51f658862d96320b8cc07496e8bda
LarsBoertjes <larsboertjes@outlook.com>
2024-02-05 12:06:22 +0100

    Update main.cpp
    added 2.4; iterating over vertices and edges

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Polygon_2.h>
+#include <CGAL/Projection_traits_yz_3.h>
+#include <CGAL/Polygon_2_algorithms.h>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef K::Point_2 Point;
-typedef CGAL::Polygon_2<K> Polygon_2;
+typedef K::Point_2                                          Point_2;
+typedef K::Point_3                                          Point_3;
+typedef CGAL::Polygon_2<K>                                  Polygon_2;
+typedef K::Segment_2                                        Segment_2;
 
-void check_inside(Point pt, Point *pgn_begin, Point *pgn_end, K traits) {
+
+void check_inside(Point_2 pt, Point_2 *pgn_begin, Point_2 *pgn_end, K traits) {
     std::cout << "The point " << pt;
     switch(CGAL::bounded_side_2(pgn_begin, pgn_end, pt, traits)) {
         case CGAL::ON_BOUNDED_SIDE :
@@ -23,16 +28,46 @@ void check_inside(Point pt, Point *pgn_begin, Point *pgn_end, K traits) {
 }
 
 int main() {
-    Point points[] = { Point(0, 0), Point(5.1, 0), Point(1,1), Point(0.5, 6)};
+    Point_2 points[] = { Point_2(0, 0), Point_2(5.1, 0), Point_2(1,1), Point_2(0.5, 6)};
     Polygon_2 pgn(points, points+4);
 
     // 2.1 check if the polygon is simple.
     std::cout << "The polygon is " << (CGAL::is_simple_2(points, points+4, K()) ? "" : "not ") << "simple." << std::endl;
 
     // 2.2 Use check_inside function declared before main()
-    check_inside(Point(0.5, 0.5), points, points+4, K());
-    check_inside(Point(1.5, 2.5), points, points+4, K());
-    check_inside(Point(2.5, 0), points, points+4, K());
+    check_inside(Point_2(0.5, 0.5), points, points+4, K());
+    check_inside(Point_2(1.5, 2.5), points, points+4, K());
+    check_inside(Point_2(2.5, 0), points, points+4, K());
 
-    return 0;
+    // 2.3 Polygons in 3D Space
+    /*Point_3 points_3d[4] = {Point_3(0,1,1), Point_3(0,2,1), Point_3(0,2,2), Point_3(0,1,2)};
+
+    bool b = CGAL::is_simple_2(points_3d, points_3d+4, CGAL::Projection_traits_yz_3<K>());
+
+    if (!b) {
+        std::cerr << "Error polygon is not simple" << std::endl;
+        return 1;
+    }*/
+
+    // 2.4 Iterating over Vertices and Edges
+    Polygon_2 p;
+    p.push_back(Point_2(0,0));
+    p.push_back(Point_2(4, 0));
+    p.push_back(Point_2(4,4));
+    p.push_back(Point_2(0,4));
+
+    std::cout << "Iterating over the vertices of p: " << std::endl;
+
+    for (int i = 0; i < p.size(); ++i) {
+        std::cout << p.vertex(i) << std::endl;
+    }
+
+    std::cout << "Iterating over the edges of p: " << std::endl;
+
+    for (int i = 0; i < p.size(); ++i) {
+        Segment_2 e = p.edge(i);
+        std::cout << e << std::endl;
+    }
+
+    return EXIT_SUCCESS;
 }
